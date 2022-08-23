01acb74b6df31d266caeda556aaf0b499f95acc2
gowtham0901 <111841582+gowtham0901@users.noreply.github.com>
2022-08-23 11:13:53 +0530

    Delete Query1 directory

--- Query1/mapper.py
+++
@@ -1,9 +0,0 @@
-#Roll:21CS60R29
-#Name:Banavathula Gowtham Naik
-#Assignment-6
-#run:cat network.txt | python mapper.py | sort | python reducer.py > result.txt
-
-import sys
-for l in sys.stdin:
-    sender = l.split() #split line
-    print ('%s\t%s' % (sender[0], 1))#employee sender count is equal to 1
--- Query1/mapper3.py
+++
@@ -1,8 +0,0 @@
-#21CS60R29
-#BANAVATHULA GOWTHAM NAIK
-#ASSIGNMENT-07
-import sys
-for l in sys.stdin:
-    l = l.strip()
-    w = l.split()
-    print ('%s \t %s'%(w[1],1))
